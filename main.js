544dda7aaa9cd86b9396aa2db80a4d6082fb5771
MuzammilSheikh786 <muzammil26122004@gmail.com>
2024-09-24 15:05:41 +0500

    operators in typescript

---
+++ main.js
@@ -0,0 +1,35 @@
+"use strict";
+//Logical Operator :
+// {AND(&&),OR(||),NOT(!)/inverter}
+// && 
+// let num1 = 20
+// let num2 = 10
+// console.log(num1!=num2 && num1>num2);
+//||
+// let num3 = 15
+// let num4 = 20
+// console.log(num3==num4 || num3<num4);
+// NOT(!)/inverter :
+// let num6 = 15
+// let num8 = 15
+// console.log(!(num6<num8));
+// Arithmetic operator:
+// + , - ,* , /
+// let x = 20
+// console.log(x + 10);
+// console.log(x - 10);
+// console.log(x * 10);
+// console.log(x / 10);
+// Assignment operator :
+// let y = 15;
+// console.log(y=9);
+// console.log(y+=1);
+// console.log(y-=1);
+// console.log(y/=3);
+// console.log(y*=2);
+// Comparision operator
+let c = 5;
+console.log(c < 3);
+console.log(c > 3);
+console.log(c >= 10);
+console.log(c == 5);
